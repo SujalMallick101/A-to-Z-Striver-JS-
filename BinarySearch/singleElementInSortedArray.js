ebadd72fb3510723ea4182a9489d40024041a3d4
sujal mallick <mallicksujal626@gmail.com>
2026-04-21 12:18:37 +0530

    Add implementation of finding the single element in a sorted array

---
+++ BinarySearch/singleElementInSortedArray.js
@@ -0,0 +1,31 @@
+let nums = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+
+function singleElement(nums) {
+    let n = nums.length;
+    if (n === 1) return nums[0];
+
+    if (nums[0] !== nums[1]) return nums[0];
+
+    if (nums[n - 1] !== nums[n - 2]) return nums[n - 1];
+
+    let low = 1;
+    let high = nums[n - 2];
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1]) {
+            return nums[mid];
+        }
+
+        if ((mid % 2 === 1 && nums[mid] === nums[mid - 1]) || (mid % 2 === 0 && nums[mid] === nums[mid + 1])) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1
+}
+
+console.log(singleElement(nums));
