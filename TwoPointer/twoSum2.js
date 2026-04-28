8c925b29b950e0557bf35a7bba849aecd238134b
sujal mallick <mallicksujal626@gmail.com>
2026-04-28 10:36:44 +0530

    implementation of two sum 2 problem

---
+++ TwoPointer/twoSum2.js
@@ -0,0 +1,25 @@
+let nums = [2, 7, 11, 15];
+let target = 9;
+
+function twoSum2(nums, target) {
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i < j) {
+        let sum = nums[i] + nums[j];
+
+        if (sum === target) {
+            return [i + 1, j + 1];
+        }
+        else if (sum > target) {
+            j--;
+        }
+        else {
+            i++;
+        }
+    }
+
+    return [-1, -1];
+}
+
+console.log(twoSum2(nums, target));
