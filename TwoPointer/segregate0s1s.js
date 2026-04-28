7deb2d5992ba4cdb142b1d069db0c7eec0d13e4b
sujal mallick <mallicksujal626@gmail.com>
2026-04-28 12:27:49 +0530

    implementation of segregate 0s and 1s

---
+++ TwoPointer/segregate0s1s.js
@@ -0,0 +1,27 @@
+let nums = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0];
+
+function segregate(nums) {
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i < j) {
+        if (nums[i] === 0 && nums[j] === 0) {
+            i++;
+        }
+        else if (nums[i] === 1 && nums[j] === 1) {
+            j--;
+        }
+        else if (nums[i] === 0 && nums[j] === 1) {
+            i++;
+            j--;
+        }
+        else {
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+    return nums;
+}
+
+console.log(segregate(nums));
