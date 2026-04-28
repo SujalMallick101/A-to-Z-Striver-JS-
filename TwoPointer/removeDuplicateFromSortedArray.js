73a1bfb7d10d8a655d12dab4a56b46a5a0f2eeae
sujal mallick <mallicksujal626@gmail.com>
2026-04-28 11:19:15 +0530

    implementation of remove duplicates from sorted array

---
+++ TwoPointer/removeDuplicateFromSortedArray.js
@@ -0,0 +1,41 @@
+let arr = [1, 1, 1, 2, 2, 3, 3];
+
+//better approach
+// function removeDuplicateFromSortedArray(arr) {
+//     let i = 0;
+//     let j = 1;
+//     let result = [];
+
+//     while (j < arr.length) {
+//         if (arr[i] === arr[j]) {
+//             j++;
+//         }
+//         else if (arr[i] !== arr[j]) {
+//             result.push(arr[i]);
+//             i = j;
+//             j++;
+//         }
+//     }
+//     result.push(arr[i]);
+//     return result;
+// }
+
+//optimal 
+function removeDuplicateFromSortedArray(arr) {
+    let i = 0;
+    let j = 1;
+
+    while (j < arr.length) {
+        if (arr[i] === arr[j]) {
+            j++;
+        }
+        else if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+            j++;
+        }
+    }
+    return arr.slice(0,i+1);
+}
+
+console.log(removeDuplicateFromSortedArray(arr));
