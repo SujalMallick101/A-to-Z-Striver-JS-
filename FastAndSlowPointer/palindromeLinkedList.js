93fae1cb48789de64b1259947f78c0474638ec11
sujal mallick <mallicksujal626@gmail.com>
2026-05-22 12:08:33 +0530

    implement findDuplicate function to detect the duplicate number in an array using the fast and slow pointer technique

---
+++ FastAndSlowPointer/palindromeLinkedList.js
@@ -0,0 +1,26 @@
+function palindrome(head) {
+    let slow=head;
+    let fast=head;
+    while(fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+
+    let prev=null;
+    while(slow!==null){
+        let next=slow.next;
+        slow.next=prev;
+        prev=slow;
+        slow=next;
+    }
+    let first=head;
+    let second=prev;
+    while(second!==null){
+        if(first.val!==second.val){
+            return false;
+        }
+        first=first.next;
+        second=second.next;
+    }
+    return true;
+}
